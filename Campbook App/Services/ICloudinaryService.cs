ba612993e6eb74f4ab9ea84f95eb18a7c60e8ec6
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-07-10 18:28:08 +0200

    Create CloudinaryService

--- Campbook App/Services/ICloudinaryService.cs
+++ Campbook App/Services/ICloudinaryService.cs
@@ -2,6 +2,7 @@ namespace Campbook_App.Services;
 
 public interface ICloudinaryService
 {
-    Task<Image> UploadAsync(IFormFile file);
-    Task DeleteAsync(string filename);
+    Task<string> UploadImageAsync(IFormFile file);
+    Task DeleteImageAsync(string publicId);
 }
+
